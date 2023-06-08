48ae2d33368440e29db644c2d6d43dd33277f8a6
Juancg <mr.juancg@gmail.com>
2023-06-08 18:16:37 +0200

    Register and Login methods
    The basic functionality for register and login has been created.

--- src/functionality/Main.java
+++ src/functionality/Main.java
@@ -1,12 +1,15 @@
 package functionality;
 
 import listener.LoginListener;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Main extends JavaPlugin {
@@ -14,6 +17,8 @@ public class Main extends JavaPlugin {
     protected PluginDescriptionFile configReader;
     protected String version;
     private ArrayList<RegisterPassword> passwords = new ArrayList<RegisterPassword>();
+    private FileConfiguration players = null;
+    private File playersFile = null;
 
     public void onEnable() {
         getLogger().info("onEnable is being called!");
@@ -23,11 +28,15 @@ public class Main extends JavaPlugin {
 
         registerEvents();
         registerCommands();
+        registerPlayers();
 
     }
 
     public void onDisable() {
         HandlerList.unregisterAll(this);
+        if (players != null) {
+            this.savePlayers();
+        }
     }
 
     public void registerCommands() {
@@ -42,8 +51,8 @@ public class Main extends JavaPlugin {
     }
 
     public RegisterPassword getRegisterPassword(String player) {
-        for(int i = 0; i<passwords.size(); i++) {
-            if(passwords.get(i).getPlayer().getName().equals(player)) {
+        for (int i = 0; i < passwords.size(); i++) {
+            if (passwords.get(i).getPlayer().getName().equals(player)) {
                 return passwords.get(i);
             }
         }
@@ -55,11 +64,52 @@ public class Main extends JavaPlugin {
     }
 
     public void deleteRegisterPass(String player) {
-        for(int i = 0; i < passwords.size(); i++) {
-            if(passwords.get(i).getPlayer().getName().equals(player)) {
+        for (int i = 0; i < passwords.size(); i++) {
+            if (passwords.get(i).getPlayer().getName().equals(player)) {
                 passwords.remove(i);
             }
         }
+    }
+
+    public void registerPlayers() {
+        playersFile = new File(this.getDataFolder(), "players.yml");
+        if (!playersFile.exists()) {
+            this.getPlayers().options().copyDefaults(true);
+            savePlayers();
+        }
+    }
+
+    public void savePlayers() {
+        try {
+            players.save(playersFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public FileConfiguration getPlayers() {
+        if (players == null) {
+            reloadPlayers();
+        }
+        return players;
+    }
+
+    public void reloadPlayers() {
+        if (players == null) {
+            playersFile = new File(getDataFolder(), "players.yml");
+        }
+        players = YamlConfiguration.loadConfiguration(playersFile);
+
+        Reader defConfigStream;
+        try {
+            defConfigStream = new InputStreamReader(this.getResource("players.yml"), "UTF8");
+            if (defConfigStream != null) {
+                YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/listener/LoginListener.java
+++ src/listener/LoginListener.java
@@ -5,6 +5,7 @@ import inv.modifier.LoginInventory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -29,16 +30,25 @@ public class LoginListener implements Listener {
     @EventHandler
     public void userJoined(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+        FileConfiguration players = plugin.getPlayers();
+        if (players.contains("Players." + player.getUniqueId() + ".pass")) {
+            loginInventory(player);
+        } else {
+            inventoryDelay(player);
+        }
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                registerInventory(player);
-            }
-        }.runTaskLater(plugin, 1L);
+
     }
 
+
     public static void registerInventory(Player player) {
+        Inventory inv = Bukkit.createInventory(null, 54, ChatColor.translateAlternateColorCodes('&', "&6PIN: &7Login"));
+        LoginInventory.fillInventory(inv);
+        plugin.addRegisterPass(player, 3);
+        player.openInventory(inv);
+    }
+
+    public static void loginInventory(Player player) {
         Inventory inv = Bukkit.createInventory(null, 54, ChatColor.translateAlternateColorCodes('&', "&6PIN: &7Register"));
         LoginInventory.fillInventory(inv);
         plugin.addRegisterPass(player, 3);
@@ -49,7 +59,7 @@ public class LoginListener implements Listener {
     public void clickInventory(InventoryClickEvent event) {
 
         String pathInventory1 = ChatColor.translateAlternateColorCodes('&', "&6&lPIN: &7Register");
-        String pathInventory2 = ChatColor.translateAlternateColorCodes('&', "&6&lPIN: &7Login");
+        String pathInventory2 = ChatColor.translateAlternateColorCodes('&', "&6PIN: &7Login");
         String pathInventory3 = ChatColor.translateAlternateColorCodes('&', "&6&lPIN: &7Modify");
         String pathinventory1M = ChatColor.stripColor(pathInventory1);
         String pathinventory2M = ChatColor.stripColor(pathInventory2);
@@ -81,15 +91,58 @@ public class LoginListener implements Listener {
                 RegisterPassword pass = plugin.getRegisterPassword(player.getName());
                 if ((slot >= 12 && slot <= 14) || (slot >= 21 && slot <= 23) || (slot >= 30 && slot <= 32)) {
                     int num = GetNum.getNumberSlot(slot);
-                    if(pass != null) {
+                    if (pass != null) {
                         pass.addPassNum(num);
                         LoginInventory.setDecorationPass(event.getClickedInventory(), pass.getPass().length());
+                        if(pass.getPass().length() >= 5) {
+                            FileConfiguration players = plugin.getPlayers();
+                            String passString = pass.getPass();
+                            if(isRegister) {
+                                players.set("Players." + player.getUniqueId() + ".pass", passString);
+                                player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                        "&6&lPin registered! &7Your pass is &a&l" + passString));
+                                plugin.deleteRegisterPass(player.getName());
+                                player.closeInventory();
+                                return;
+                            } else if(isLogged) {
+                                String userPass = players.getString("Players." + player.getUniqueId() + ".pass");
+                                if(passString.equals(userPass)) {
+                                    player.playSound(player.getLocation(), Sound.LEVEL_UP, 10, 2);
+                                    player.sendMessage(ChatColor.translateAlternateColorCodes('&',
+                                            "&a&lYou're logged in!"));
+                                    plugin.deleteRegisterPass(player.getName());
+                                    player.closeInventory();
+                                    return;
+                                } else {
+                                    int attemps = pass.getAttempts();
+                                    if(attemps <= 1) {
+                                        plugin.deleteRegisterPass(player.getName());
+                                        player.kickPlayer(ChatColor.translateAlternateColorCodes('&',
+                                                "&cYou have exceeded the attempt limit!"));
+                                        return;
+                                    } else {
+                                        player.playSound(player.getLocation(), Sound.FIZZ, 10, 1);
+                                        int maxAttempts = pass.getMaxAttempts();
+                                        pass.reduceAttempts();
+                                        LoginInventory.resetDecorationPass(event.getClickedInventory());
+                                        pass.resetPass();
+                                        player.kickPlayer(ChatColor.translateAlternateColorCodes('&',
+                                                "&c&lWrong pin! &8(&6"+(maxAttempts - attemps+1)+"&7/&6"+maxAttempts+"&8)"));
+                                        return;
+                                    }
+                                }
+
+                            } else if (isModify) {
+
+                            }
+                        }
+
 
 
                         player.playSound(player.getLocation(), Sound.NOTE_PLING, 10, 2);
                     }
-                } else if(slot == 49) {
-                    if(pass != null) {
+                } else if (slot == 49) {
+                    if (pass != null) {
                         pass.resetPass();
                         LoginInventory.resetDecorationPass(event.getClickedInventory());
                     }
@@ -102,7 +155,7 @@ public class LoginListener implements Listener {
     public void closeInventory(InventoryCloseEvent event) {
         Player player = (Player) event.getPlayer();
         Inventory inv = event.getInventory();
-        if(plugin.getRegisterPassword(player.getName()) != null) {
+        if (plugin.getRegisterPassword(player.getName()) != null) {
             Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
                 @Override
                 public void run() {
@@ -111,6 +164,15 @@ public class LoginListener implements Listener {
             }, 1L);
         }
 
+    }
+
+    public void inventoryDelay(Player player) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                registerInventory(player);
+            }
+        }.runTaskLater(plugin, 1L);
     }
 
 
