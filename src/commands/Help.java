6d4e12460b59e8f8be3d16103a8534cb3e1dec49
Juancg <mr.juancg@gmail.com>
2023-06-13 01:52:47 +0200

    The /lc-help command has been modified
    - Now you can read the description of the command by hovering the mouse over the command.
    - If you left-click on the command, you will get a suggestion in the chat to send the command.

--- src/commands/Help.java
+++ src/commands/Help.java
@@ -2,17 +2,26 @@ package commands;
 
 import functionality.Main;
 import messages.CommandMessages;
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.awt.*;
+
 public class Help implements CommandExecutor {
 
     private Main plugin;
     private Player player;
+    TextComponent modifyPin = new TextComponent();
+    TextComponent lcVersion = new TextComponent();
+    TextComponent lcReload = new TextComponent();
 
     public Help(Main plugin) {
         this.plugin = plugin;
@@ -26,12 +35,15 @@ public class Help implements CommandExecutor {
         } else {
             player = (Player) commandSender;
             if (command.getName().equalsIgnoreCase("lc-help")) {
+                modifyPinTxt(modifyPin);
+                versionTxt(lcVersion);
+                reloadTxt(lcReload);
 
                 player.playSound(player.getLocation(), Sound.ANVIL_USE, 10, 2);
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&m-------------&8[&f&lLock&6&lCraft&8]&f&m-------------"));
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7- &e/modifypin &8- &7Change your pin."));
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7- &e/lc-version &8- &7Check the plugin version."));
-                player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7- &e/lc-reload &8- &c(Admin) &7Reload plugin."));
+                player.spigot().sendMessage(modifyPin);
+                player.spigot().sendMessage(lcVersion);
+                player.spigot().sendMessage(lcReload);
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&m-------------------------------------"));
 
                 return true;
@@ -41,4 +53,29 @@ public class Help implements CommandExecutor {
             }
         }
     }
+
+    public void modifyPinTxt(TextComponent msg) {
+
+        msg.setText(ChatColor.translateAlternateColorCodes('&', "&7- &e/modifypin"));
+        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(
+                "Change your old PIN to a new one.").color(ChatColor.AQUA).create()));
+        msg.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/modifypin"));
+    }
+
+    public void versionTxt(TextComponent msg) {
+
+        msg.setText(ChatColor.translateAlternateColorCodes('&', "&7- &e/lc-version"));
+        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(
+                "Check the plugin version.").color(ChatColor.AQUA).create()));
+        msg.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/lc-version"));
+    }
+
+    public void reloadTxt(TextComponent msg) {
+
+        msg.setText(ChatColor.translateAlternateColorCodes('&', "&7- &e/lc-reload"));
+        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(
+                "Reload the plugin.").color(ChatColor.AQUA).create()));
+        msg.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/lc-reload"));
+    }
+
 }
